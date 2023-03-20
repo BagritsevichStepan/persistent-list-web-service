39df6f9633d1cb1af63335e651fd34f92f22448f
Stsiapan Bahrytsevich <stepan.bahrytsevich@tum.de>
2023-03-20 20:05:44 +0100

    Create project

---
+++ src/main/kotlin/com/wmms/task3/WmmsTestTask3Application.kt
@@ -0,0 +1,11 @@
+package com.wmms.task3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WmmsTestTask3Application
+
+fun main(args: Array<String>) {
+    runApplication<WmmsTestTask3Application>(*args)
+}
---
+++ src/test/kotlin/com/wmms/task3/WmmsTestTask3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wmms.task3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WmmsTestTask3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
